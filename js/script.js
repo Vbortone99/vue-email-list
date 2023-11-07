9e3be86523da5ccf6d5fe2062298ae69d7f1677d
Vbortone99 <bortonevincenzo99@gmail.com>
2023-11-07 21:43:07 +0100

    10 e-mails generated

--- js/script.js
+++ js/script.js
@@ -10,9 +10,11 @@ createApp({
 
     },
     mounted(){
-        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp)=>{
-            console.log(resp.data.response);
-            
-        })
+        for(let x = 0; x < 10; x++){
+            axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp)=>{
+                // console.log(resp.data.response);
+                this.emails.push(resp.data.response);
+            });
+        }
     }
 }).mount('#app')
